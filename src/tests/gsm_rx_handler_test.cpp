6a1ee9bed4dd2efb35006fca97d51f91f5e7fcbc
Mateusz Lubecki <sp8ebc@interia.pl>
2022-02-16 22:14:11 +0100

    synced with paratnc

--- src/tests/gsm_rx_handler_test.cpp
+++ src/tests/gsm_rx_handler_test.cpp
@@ -7,7 +7,8 @@
 
 
 extern "C" {
-#include "gsm/gsm.h"
+#include "gsm/sim800c.h"
+#include "gsm/sim800c_inline.h"
 #include "stdperiph_stubs.h"
 #include "rte_wx.h"
 #include "other_stubs.h"
@@ -15,6 +16,49 @@ extern "C" {
 
 
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE ANALOG_ANEMOMETER
+#define BOOST_TEST_MODULE GSM_RX_HANDLER
 #include <boost/test/unit_test.hpp>
+#include <string>
+#include <iostream>
 
+struct fixture {
+	fixture() {
+		//;
+	}
+};
+
+BOOST_FIXTURE_TEST_CASE(check_for_extra_newlines_1, fixture) {
+	std::string input = "\r\nNO CARRIER\r\nAT\r\nOK\r\n\r\nRING\r\n\r\nRING\r\n\r\nRING\r\nAT+CREG?\r\n+CREG: 0,1\r\n\r\nOK\r\n\r\nRING\r\n\r\nRING\r\n\r\nNO CARRIER\r\n";
+
+	uint32_t result = gsm_sim800_check_for_extra_newlines(reinterpret_cast<uint8_t *>(const_cast<char *>(input.c_str())), input.length());
+
+	BOOST_CHECK(result != 0);
+}
+
+BOOST_FIXTURE_TEST_CASE(look_for_ring_1, fixture) {
+	std::string input = "\r\nRING\r\nAT+CREG?\r\n+CREG: 0,1\r\n\r\nOK\r\n\r\nRING\r\n\r\nRING\r\n\r\nNO CARRIER\r\n";
+
+	uint16_t offset = 0;
+
+	uint32_t result = gsm_sim800_check_for_extra_newlines(reinterpret_cast<uint8_t *>(const_cast<char *>(input.c_str())), input.length());
+
+	std::string substr = input.substr(result & 0xFF);
+
+	gsm_sim800_check_for_async_messages(reinterpret_cast<uint8_t *>(const_cast<char *>(substr.c_str())), substr.length(), &offset);
+
+	BOOST_CHECK_EQUAL(offset, 6);
+}
+
+BOOST_FIXTURE_TEST_CASE(look_for_nocarrier_1, fixture) {
+	std::string input = "\r\nAT\r\nNO CARRIER\r\nRING\r\n\r\nOK\r\n\r\nRING\r\n\r\nRING\r\n\r\nRING\r\nAT+CREG?\r\n+CREG: 0,1\r\n\r\nOK\r\n\r\nRING\r\n\r\nRING\r\n\r\nNO CARRIER\r\n";
+
+	uint16_t offset = 0;
+
+	uint32_t result = gsm_sim800_check_for_extra_newlines(reinterpret_cast<uint8_t *>(const_cast<char *>(input.c_str())), input.length());
+
+	std::string substr = input.substr(result & 0xFF);
+
+	gsm_sim800_check_for_async_messages(reinterpret_cast<uint8_t *>(const_cast<char *>(substr.c_str())), substr.length(), &offset);
+
+	BOOST_CHECK_EQUAL(offset, 6);
+}
