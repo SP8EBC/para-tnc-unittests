c83b63d24b1c622c648fd5af587ac0c7218296c3
Mateusz Lubecki <sp8ebc@interia.pl>
2022-03-19 13:37:53 +0100

    unit tests for http client terminating callback

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -6,6 +6,7 @@
 C_SRCS += \
 ../src/analog_anemometer.c \
 ../src/davis_parsers.c \
+../src/http_client_rx_callback.c \
 ../src/io.c \
 ../src/sim800c.c \
 ../src/sim800c_engineering.c \
@@ -15,6 +16,7 @@ C_SRCS += \
 OBJS += \
 ./src/analog_anemometer.o \
 ./src/davis_parsers.o \
+./src/http_client_rx_callback.o \
 ./src/io.o \
 ./src/sim800c.o \
 ./src/sim800c_engineering.o \
@@ -24,6 +26,7 @@ OBJS += \
 C_DEPS += \
 ./src/analog_anemometer.d \
 ./src/davis_parsers.d \
+./src/http_client_rx_callback.d \
 ./src/io.d \
 ./src/sim800c.d \
 ./src/sim800c_engineering.d \
